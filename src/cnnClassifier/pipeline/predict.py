f7446f4c69ef3f1f13bd5b44b4d7086d946ececc
Your Name <vedantpmokati@gmail.com>
2024-01-21 19:07:49 +0530

    changed

--- src/cnnClassifier/pipeline/predict.py
+++ src/cnnClassifier/pipeline/predict.py
@@ -22,7 +22,7 @@ class PredictionPipeline:
         result = np.argmax(model.predict(test_image), axis=1)
         print(result)
 
-        if result[0] == 1:
+        if (result[0] == 1):
             prediction = 'Healthy'
             return [{ "image" : prediction}]
         else:
